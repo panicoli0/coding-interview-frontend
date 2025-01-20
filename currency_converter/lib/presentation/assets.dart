ecacf9d59b8b47b4d5d28e6041e6fb8c29e6c2bc
pablo-nicoli-ueno <pablo.nicoli@itti.digital>
2025-01-20 16:44:46 +0400

    created const for Assets

---
+++ currency_converter/lib/presentation/assets.dart
@@ -0,0 +1,11 @@
+class Assets {
+  static const String baseCrypto = 'assets/cripto_currencies/';
+  static const String baseFiat = 'assets/fiat_currencies/';
+
+  static const String tatum = '${baseCrypto}TATUM-TRON-USDT.png';
+
+  static const String brlCurrency = '${baseFiat}BRL.png';
+  static const String copCurrency = '${baseFiat}COP.png';
+  static const String penCurrency = '${baseFiat}PEN.png';
+  static const String vesCurrency = '${baseFiat}VES.png';
+}
